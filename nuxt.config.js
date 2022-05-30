87fefd5d6d7eef22cb6362b697edb7acae3016b8
WoodNeck <saiyu915@naver.com>
2022-05-30 11:52:25 +0900

    Init

---
+++ nuxt.config.js
@@ -0,0 +1,33 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-test',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    "@egjs/flicking/dist/flicking.css"
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  server: {
+    host: "0"
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <Flicking >
+    <div class="panel">1</div>
+    <div class="panel">2</div>
+    <div class="panel">3</div>
+  </Flicking>
+</template>
+
+<script>
+import { Flicking } from "@egjs/vue-flicking";
+
+export default {
+  name: 'IndexPage',
+  components: {
+    Flicking
+  }
+}
+</script>
+<style>
+.panel {
+  width: 200px;
+  height: 200px;
+}
+</style>
